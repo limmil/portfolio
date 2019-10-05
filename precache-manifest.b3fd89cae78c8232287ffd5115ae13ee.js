c0bdaf364da740dc078a275c49cf9332f3b4a99e
Min <leecosecant@gmail.com>
2019-10-05 12:49:49 -0700

    Updates

---
+++ precache-manifest.b3fd89cae78c8232287ffd5115ae13ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88a1f9e3ba2ccf60bfe51bbe35ceca8a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "1ff111172b4b735a72a7",
+    "url": "/portfolio/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "e6b99aefc985d0445f73",
+    "url": "/portfolio/static/css/main.28798d99.chunk.css"
+  },
+  {
+    "revision": "1ff111172b4b735a72a7",
+    "url": "/portfolio/static/js/2.c63636ad.chunk.js"
+  },
+  {
+    "revision": "e6b99aefc985d0445f73",
+    "url": "/portfolio/static/js/main.4214b1c8.chunk.js"
+  },
+  {
+    "revision": "40fcc410fcaa1ed35721",
+    "url": "/portfolio/static/js/runtime~main.1a68cab5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.2b192f9a0afd8ead01713195d88de52a.js"
+  "/portfolio/precache-manifest.b3fd89cae78c8232287ffd5115ae13ee.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
